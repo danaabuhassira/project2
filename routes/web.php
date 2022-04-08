ac5550a9af2dfcd6082b98a7c129c1894fa29b1b
unknown <danaabuhassira@gmail.com>
2022-04-09 01:48:00 +0300

    first commit

---
+++ routes/web.php
@@ -0,0 +1,10 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TaskController;
+
+Route::get('/',[TaskController::class, 'index']);
+Route::post('/store',[TaskController::class,'store']);
+Route::post('delete/{id}',[TaskController::class,'delete']);
+
+
